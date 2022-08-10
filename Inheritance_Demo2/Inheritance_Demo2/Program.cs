c58f81316d8fddfd49e632b1e05c6726eb15f7a6
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-09 23:41:56 -0500

    add inheritance demo 2

---
+++ Inheritance_Demo2/Inheritance_Demo2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Inheritance_Demo2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
