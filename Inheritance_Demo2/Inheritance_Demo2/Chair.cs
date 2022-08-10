416dcb041c8a49be32b6496530ddde9a1c48b764
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-09 23:52:18 -0500

    update inheritance demo - add classes

---
+++ Inheritance_Demo2/Inheritance_Demo2/Chair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Demo2 {
+
+    // subclass chair extends Furniture
+    internal class Chair : Furniture {
+        
+        // simple constructor
+        public Chair() {
+            this.Color = color;
+            this.Material = material;
+        }
+
+    }
+}
---
+++ Inheritance_Demo2/Inheritance_Demo2/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Demo2 {
+    internal class Vehicle {
+    }
+}
