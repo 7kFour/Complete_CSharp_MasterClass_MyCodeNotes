f2c09dad72fa17a8b825b555535ffc64845ebfd7
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-11 15:18:01 -0500

    add more interface demos

---
+++ IEnumerable_IEnumerator_Demo/IEnumerable_IEnumerator_Demo/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace IEnumerable_IEnumerator_Demo {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Simple_InheritanceInterface_Demo/Simple_InheritanceInterface_Demo/Program.cs
@@ -0,0 +1,64 @@
+﻿using Coding.Exercise;
+using System;
+
+//Create An interface called IShootable.It will contain a method Shoot;
+//Create a class Weapon with a Name attribute and method Label that will display the name of a weapon
+//in the format "This is X!", where X is a name of a weapon;
+//Create a class Gun that inherits from Weapon, can shoot with a "Bang!" message, has the name "Gun",
+//and is able to show the label. 
+
+namespace Coding.Exercise {
+
+    // putting classes and interface in Program.cs for readability
+
+    // implementing interface 
+    internal interface IShootable {
+        void Shoot();
+    }
+
+    // weapon class
+    internal class Weapon {
+
+        // properties
+        protected string Name { get; set; }
+
+        // constructors
+        public Weapon() {
+            Name = "Hammer";
+        }
+
+        // methods
+        public void Label() {
+            Console.WriteLine($"This is {Name}!");
+        }
+    }
+
+    // Gun class inherits from Weapon and implements IShootable interface
+    internal class Gun : Weapon, IShootable {
+
+        // constructors
+        public Gun() {
+            Name = "Gun";
+        }
+
+        // methods
+        public void Shoot() {
+            Console.WriteLine("Bang!");
+        }
+    }
+
+    public static class Program {
+        static public void Main(string[] args) {
+            // new instance 
+            Gun pist = new Gun();
+
+            // test for methods
+            pist.Label();
+            pist.Shoot();
+
+            // verifying the interface and the parent class
+            if (pist is IShootable && pist is Weapon)
+                System.Console.WriteLine("Yes, it is my parents.");
+        }
+    }
+}
