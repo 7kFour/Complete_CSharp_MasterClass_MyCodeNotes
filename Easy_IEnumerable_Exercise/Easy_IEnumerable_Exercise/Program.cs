7c0a4272e4d313a7b1cf9988476b1c92c95342be
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-12 13:26:25 -0500

    finish another ienum chall

---
+++ Easy_IEnumerable_Exercise/Easy_IEnumerable_Exercise/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Easy_IEnumerable_Exercise {
+    // TODO
+    // create Contact and PhoneBook classes in a way that will allow the 
+    // Main method provided to function properly 
+    // Use the contact list
+    //new Contact("Andre", "435797087"),
+    //                new Contact("Lisa", "435677087"),
+    //                new Contact("Dine", "3457697087"),
+    //                new Contact("Sofi", "4367697087")
+    //            };
+    // method should be named Call and should print "Calling to X. Phone numer is Y"
+    // where X is name and Y is phone number
+
+    public static class Program {
+        static public void Main(string[] args) {
+            PhoneBook MyPhoneBook = new PhoneBook();
+
+            foreach (Contact contact in MyPhoneBook) {
+                contact.Call();
+            }
+        }
+    }
+
+    class Contact { 
+        
+        public string Name { get; set; }
+        public string Number { get; set; }
+
+        public Contact(string name, string number) {
+            Name = name;
+            Number = number;
+        }
+
+        public void Call() {
+            Console.WriteLine($"Calling to {Name}. Phone number is {Number}");
+        }
+    }
+
+    // want to be able to iterate through Contacts so creating 
+    // a list of IEnumerable<Contact>
+    class PhoneBook : IEnumerable<Contact> {
+
+        // declaring a list of Contacts for the constructor
+        // to initialize
+        public List<Contact> theList;
+
+        // basic constructor - initializes List<T> collection
+        public PhoneBook() {
+            theList = new List<Contact>() {
+                    new Contact("Andre", "435797087"),
+                    new Contact("Lisa", "435677087"),
+                    new Contact("Dine", "3457697087"),
+                    new Contact("Sofi", "4367697087")
+            };
+        }
+
+        public IEnumerator<Contact> GetEnumerator() {
+            return theList.GetEnumerator();
+        }
+
+        // not using a non-generic so don't need to implement logic
+        // do need the method signature to stay however to fulfill the 
+        // interface 
+        IEnumerator IEnumerable.GetEnumerator() {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
---
+++ IEnumerableDemo3/IEnumerableDemo3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerableDemo3 {
+
+    // in OOP / Inheritance a Child of a Parent can be passed as an argument
+    // to a method the same as the Parent would be
+    // passing an IEnumerable<T> to a method - is similar - in that we can 
+    // pass collections that implement the IEnumerable<T> interface
+
+
+    internal class Program {
+        static void Main(string[] args) {
+
+            // a list of type List<int> initialized with some ints 
+            List<int> numberList = new List<int>() { 8, 6, 2 };
+
+            // array of type int[] intialized with some ints
+            int[] numArr = new int[] { 1, 7, 1, 3 };
+
+            CollectionSum(numberList);
+            CollectionSum(numArr);
+        }
+
+        static void CollectionSum(IEnumerable<int> anyCollection) {
+
+            // sum variable to store sum of ints in anyCollection
+            int sum = 0;
+
+            // for each number in the collection add its value to sum
+            foreach (int n in anyCollection) {
+                sum += n;
+            }
+
+            // print the sum
+            Console.WriteLine($"The sum is: {sum}");
+        }
+    }
+}
