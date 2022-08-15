c11df05b31900b0ec9f86193e957c0ac721e9666
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-14 19:03:02 -0500

    start abstract classes demo

---
+++ AbstractClasses_Demo/AbstractClasses_Demo/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AbstractClasses_Demo {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
