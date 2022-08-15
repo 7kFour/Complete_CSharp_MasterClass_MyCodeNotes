d44d041b4eaee61d80fce930195b25ce2b5173ca
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-15 12:45:22 -0500

    abstract classes - initial commit

--- AbstractClasses_Demo/AbstractClasses_Demo/Program.cs
+++ AbstractClasses_Demo/AbstractClasses_Demo/Program.cs
@@ -3,7 +3,7 @@
 namespace AbstractClasses_Demo {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
