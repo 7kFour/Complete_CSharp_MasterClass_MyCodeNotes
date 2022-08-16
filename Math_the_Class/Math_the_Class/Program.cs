96bd63ecc99a2561708b78f847b6520b7e60c922
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-16 16:19:57 -0500

    math, random classes finish, start regex

--- Math_the_Class/Math_the_Class/Program.cs
+++ Math_the_Class/Math_the_Class/Program.cs
@@ -3,7 +3,46 @@
 namespace Math_the_Class {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            // Math has many mathematical functions built in -- just enter Math. to see many
+            // https://docs.microsoft.com/en-us/dotnet/api/system.math?view=net-6.0
+
+            // few examples - mess around with the Class to see what else it can do
+            // if you're working with math remember this Class it can save a lot of time
+
+            // forced rounding up - ceiling - get next whole number up
+            Console.WriteLine("Ceiling: " + Math.Ceiling(15.3)); // 16 
+
+            // forced round down - floor - get next whole number down
+            Console.WriteLine("Ceiling: " + Math.Ceiling(15.7)); // 15
+
+            int num1 = 13;
+            int num2 = 9;
+
+            // get lower of 2 values
+            Console.WriteLine($"Lower of {num1} and {num2} is {Math.Min(num1, num2)}"); // Lower of 13 and 9 is 9
+
+            // get higher of 2 values
+            Console.WriteLine($"Higher of {num1} and {num2} is {Math.Max(num1, num2)}"); // Higher of 13 and 9 is 13
+
+            // Power
+            Console.WriteLine($"3 to the power of 5 is {Math.Pow(3, 5)}"); // 3 to the power of 5 is 243
+
+            // Math has the constants Pi and E (natural logarithmic base)
+            // only a specific precision for Pi and E - what fits in a double
+            Console.WriteLine($"Pi: {Math.PI}"); // Pi: 3.141592653589793
+            Console.WriteLine($"E: {Math.E}"); // E: 2.718281828459045
+
+            // square root 
+            Console.WriteLine($"Square root of 25 is {Math.Sqrt(25)}"); // Square root of 25 is 5
+
+            // Absolute number - always positive 
+            Console.WriteLine($"-25 as an absolute is {Math.Abs(-25)}"); // -25 as an absolute is 25
+
+            // Cosine
+            Console.WriteLine($"Cosine of 1 is {Math.Cos(1)}"); // Cosine of 1 is 0.5403023058681398
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Random_Class/Random_Class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Random_Class {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // create and initialize a new object of type Random
+            Random dice = new Random();
+
+            // number of eyes on dice
+            int numEyes = 0;
+
+            for (int i = 0; i < 10; i++) {
+
+                // Next(min, max) can also have just a max value
+                // max is exclusive - this is a 6 sided dice
+                numEyes = dice.Next(1, 7);
+                Console.Write($"{numEyes} "); // 6 5 1 1 4 2 2 4 3 6
+            }
+
+            Console.WriteLine("\nYes, No, Maybe question answer thing");
+            Random yesNoMaybe = new Random();
+            int ansNum = dice.Next(1, 4);
+
+            switch (ansNum) {
+                case 1:
+                    Console.WriteLine("yes");
+                    break;
+                case 2:
+                    Console.WriteLine("no");
+                    break;
+                case 3:
+                    Console.WriteLine("maybe");
+                    break;
+                default:
+                    Console.WriteLine("uhh");
+                    break;
+            }
+        }
+    }
+}
