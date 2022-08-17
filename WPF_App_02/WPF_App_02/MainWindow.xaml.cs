e24cbb26ca8ad9334294e3f6302aa9ff989e0b1d
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-17 12:12:03 -0500

    start WPF section

--- WPF_App_02/WPF_App_02/MainWindow.xaml.cs
+++ WPF_App_02/WPF_App_02/MainWindow.xaml.cs
@@ -14,12 +14,55 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WPF_App_02 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
+    // this is a recreation of the XAML code with C# 
+
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            // creating a new grid and initializing it to content
+            // this.Content - is basically the content of the application
+            // in the design window is this basically everything between the <Window></Window> tag
+            // basically saying to add a new <Grid></Grid> element inside the window tags
+            Grid grid = new Grid();
+            this.Content = grid;
+
+            // creating a button with code behind
+            Button btn = new Button();
+            btn.FontSize = 26;
+            btn.Width = 250;
+            btn.Height = 100;
+
+            // create a wrap panel for button like in the XML
+            WrapPanel wp = new WrapPanel();
+
+            // adding a text block
+            // text color is done with the Brushes class
+            TextBlock txt = new TextBlock();
+            txt.Text = "Multi";
+            txt.Foreground = Brushes.Blue;
+
+            // adding the text to the wrap panel
+            wp.Children.Add(txt);
+
+            // adding the other text to recreate the multicolor text from the XAML
+            // must assign a new text block to txt each time we want to add new content
+            txt = new TextBlock();
+            txt.Text = "Color";
+            txt.Foreground = Brushes.Red;
+            wp.Children.Add(txt);
+
+            txt = new TextBlock();
+            txt.Text = "Button";
+            txt.Foreground = Brushes.White;
+            wp.Children.Add(txt);
+
+            // add the wrap panel to the button 
+            btn.Content = wp;
+
+            // adding the button to the Grid - basically the body if you think of <html><body></body></html>
+            grid.Children.Add(btn);
         }
     }
 }
