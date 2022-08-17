e086b21c20414c8c0466054f9f8607bfa6e99d48
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-17 17:09:26 -0500

    exercise 13 math

---
+++ Ex13_Math_Class/Ex13_Math_Class/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ex13_Math_Class {
+    internal class Program {
+        static void Main(string[] args) {
+            Run("12");
+        }
+
+        public static double ConvertToRadians(double angle) {
+            return (Math.PI / 180) * angle;
+        }
+
+        public static void Run(string line) {
+            // try parse string and output to n
+            int n;
+            bool success = int.TryParse(line, out n);
+
+            // convert int to radian value and make double
+            double radN = ConvertToRadians(n);
+
+            // if int was parsed and is between 0-180 
+            // calculate Sine, Cosine and Tanger
+            // else - Check the Input
+            if (success && n >= 0 && n <= 180) {
+                Console.WriteLine($"Cos = {Math.Cos(radN)}");
+                Console.WriteLine($"Sine = {Math.Sin(radN)}");
+                Console.WriteLine($"Tg = {Math.Tan(radN)}");
+            } else {
+                Console.WriteLine("Check the input!");
+            }
+        }
+    }
+}
