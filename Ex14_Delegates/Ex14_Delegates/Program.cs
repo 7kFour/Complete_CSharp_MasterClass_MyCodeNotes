7b335a584d76124f86eaec17961f92d0c47d82ba
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-18 18:42:53 -0500

    finish ex14

--- Ex14_Delegates/Ex14_Delegates/Program.cs
+++ Ex14_Delegates/Ex14_Delegates/Program.cs
@@ -5,7 +5,7 @@ namespace Ex14_Delegates {
 
         // Create public delegate called OperationDelegate
         // takes 2 arguments of float type and returns a float type
-        
+
         // Create 4 static public methods called Add, Subtract, Multiple, and Divide
         // each takes 2 arguments of float type and returns a float type value 
         // corresponding to the result of the performed operation
@@ -14,8 +14,36 @@ namespace Ex14_Delegates {
         // Create method ApplyOperation that takes 2 float arguments and one delegate 
         // and applies the operation to those values 
 
+        public delegate float OperationDelegate(float numOne, float numTwo);
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            // creating variables for readability when printing
+            float a = ApplyOperation(15, 20, Add);
+            float s = ApplyOperation(100, 80, Subtract);
+            float m = ApplyOperation(5, 10, Multiply);
+            float d = ApplyOperation(40, 2, Divide);
+
+            Console.WriteLine($"15 + 20 = {a}\n100 - 80 = {s}" +
+                $"\n5 * 10 = {m}\n40 / 2 = {d}");
+            
+        }
+
+        public static float ApplyOperation(float x, float y, OperationDelegate o) {
+            return o(x, y);
+        }
+
+        public static float Add(float x, float y) {
+            return x + y;
+        }
+
+        public static float Subtract(float x, float y) {
+            return x - y;
+        }
+        public static float Multiply(float x, float y) {
+            return x * y;
+        }
+        public static float Divide(float x, float y) {
+            return x / y;
         }
     }
 }
