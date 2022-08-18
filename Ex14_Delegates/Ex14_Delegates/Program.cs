467945d98c733c70d5c923c6f8fcfe30955befd1
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-18 16:46:18 -0500

    add ex14

---
+++ Ex14_Delegates/Ex14_Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex14_Delegates {
+    internal class Program {
+
+        // Create public delegate called OperationDelegate
+        // takes 2 arguments of float type and returns a float type
+        
+        // Create 4 static public methods called Add, Subtract, Multiple, and Divide
+        // each takes 2 arguments of float type and returns a float type value 
+        // corresponding to the result of the performed operation
+        // eg Add(float x, float y) { return x+y }
+
+        // Create method ApplyOperation that takes 2 float arguments and one delegate 
+        // and applies the operation to those values 
+
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
