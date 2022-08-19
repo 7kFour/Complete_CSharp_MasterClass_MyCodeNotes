df3aeb4f21b8030dd35c9867a628e2cf2c31ebeb
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-19 16:32:19 -0500

    start regex section

---
+++ RegEx_RegularExpressions/RegEx_RegularExpressions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RegEx_RegularExpressions {
+    internal class Program {
+
+        // RegEx is very useful - used in most programming languages
+        // ctrl+f then click the square with a * beside it or alt+e lets you search regex in visual studio
+        // you can use this to test the regex you create easily
+
+        // check Snippets.txt for many useful regex 
+
+        // https://docs.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
+        // https://regex101.com/ -- good tool for creating and testing RegEx
+
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
