86a0a35d04854cb8103f71b421fa3a9e1bcd5834
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-22 21:48:01 -0500

    game

--- Parsing_Game_Pt1/Parsing_Game_Pt1/Program.cs
+++ Parsing_Game_Pt1/Parsing_Game_Pt1/Program.cs
@@ -1,18 +1,22 @@
 ﻿using System;
+using System.IO;
 
 namespace Parsing_Game_Pt1 {
     internal class Program {
 
         // the rules 
         // Read from the input.txt line by line
-        // If one of the lines contains "split", you have to split it using 
-        //  the split method from the string class
-        // Take the return from split and write the element with index 4 into the output.txt
+        // Split() the text into an array
+        // Take the returned elements from split and write the element with index 4 into the output.txt
         // Add a space between each element to make it readable
         // If your output is not a readable sentence, your answer is wrong
 
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            // read entire file to a string
+            string[] lines = System.IO.File.ReadAllLines(@"D:\dev\c#\Complete_CSharp_MasterClass_MyCodeNotes\Parsing_Game_Pt1\Parsing_Game_Pt1\input.txt");
+
+            
         }
     }
 }
