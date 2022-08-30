b68cc9a3f9750f12d34cae5c316fd1fe993f3bf1
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-30 16:34:11 -0500

    finish app 11 - checkboxes

--- WPF_App_11/WPF_App_11/MainWindow.xaml.cs
+++ WPF_App_11/WPF_App_11/MainWindow.xaml.cs
@@ -21,5 +21,28 @@ namespace WPF_App_11 {
         public MainWindow() {
             InitializeComponent();
         }
+
+        private void cbAllCheckedChanged(object sender, RoutedEventArgs e) {
+            // if All Toppings is checked then all other check boxes should be checked or unchecked 
+            bool newVal = (cbAllToppings.IsChecked == true);
+            cbSalami.IsChecked = newVal;
+            cbMushrooms.IsChecked = newVal;
+            cbMozzarella.IsChecked = newVal;    
+        }
+
+        private void cbSingleCheckedChanged(object sender, RoutedEventArgs e) {
+            // values are either checked or null
+            cbAllToppings.IsChecked = null;
+
+            if ((cbSalami.IsChecked == true) && (cbMushrooms.IsChecked == true) && (cbMozzarella.IsChecked == true)) {
+                // if all 3 of the options are checked then All Toppings should be checked 
+                cbAllToppings.IsChecked = true;
+            }
+
+            if ((cbSalami.IsChecked == false) && (cbMushrooms.IsChecked == false) && (cbMozzarella.IsChecked == false)) {
+                // if all 3 of the options are unchecked then All Toppings should be unchecked 
+                cbAllToppings.IsChecked = false;
+            }
+        }
     }
 }
