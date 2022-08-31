fa35d2279bc741d2b6b0c2937be804d6f2aa1228
Izalith <48108297+7kFour@users.noreply.github.com>
2022-08-31 13:24:01 -0500

    finish 13 14 15

---
+++ WPF_App_15/WPF_App_15/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_App_15 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        // message box that welcomes user when they press login 
+        // if they don't type anything in the uname it will just say welcome
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show($"Welcome {tbUserName.Text}");
+        }
+    }
+}
