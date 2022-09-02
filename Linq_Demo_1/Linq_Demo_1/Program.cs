5f5bdba0a76a3bc59101026ddb1399d733ad8488
Izalith <48108297+7kFour@users.noreply.github.com>
2022-09-01 22:18:43 -0500

    start linq section

---
+++ Linq_Demo_1/Linq_Demo_1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Linq_Demo_1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
